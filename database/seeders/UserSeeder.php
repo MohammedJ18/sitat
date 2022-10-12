3c0d0c9517aaccb66d146afcdd94c59f7d352332
fatmanawfal <fatma.nawfal.abdalrazak@gmail.com>
2022-10-12 20:59:07 +0300

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,9 +15,17 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        // id = 1
         User::create([
             'name' => 'Mohammed',
             'email' => 'mohammed@gamil.com',
+            'password' => bcrypt('121212'),
+        ]);
+
+        // id = 2
+        User::create([
+            'name' => 'Fatma Nawfal',
+            'email' => 'fatma.nawfal@gamil.com',
             'password' => bcrypt('121212'),
         ]);
     }
