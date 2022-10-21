134c1f345df09efd048e59a39da71659c146fa71
Mohammed Jawad <jawad6686@gmail.com>
2022-10-21 20:22:57 +0300

    add

---
+++ app/Http/Livewire/Pages/Services/ServiceProfile.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Pages\Services;
+
+use Livewire\Component;
+
+class ServiceProfile extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.services.service-profile');
+    }
+}
